9e6e8f9f8851fa53f7645658897e254c42632f40
michael.jirasek@glasgow.ac.uk <michael.jirasek@glasgow.ac.uk>
2023-01-19 23:17:04 +0000

    refactor: solved integral

--- run.py
+++ run.py
@@ -14,18 +14,22 @@ from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
 
-file_path = filedialog.askopenfilename(
-  title="lel",
-  filetypes=(
-      ("jpeg files", "*.jpg"),
-      ("csv files", "*.csv"),
-      ("gif files", "*.gif*"),
-      ("png files", "*.png")
-    )
-  )
+# file_path = filedialog.askopenfilename(
+#   title="Select geometry xyz file.",
+#   initialdir='.',
+#   filetypes=(
+#       # ("jpeg files", "*.jpg"),
+#       # ("csv files", "*.csv"),
+#       ("xyz files", "*.xyz"),
+#       ("gif files", "*.gif*")
+#       # ("png files", "*.png")
+#     )
+#   )
 
+file_path = 'C:/Users/edu51/MJ_Python/rcm-python/cp6t6_0p_blyp35_geom.xyz'
 
-
+from rdkit import Chem
+a = Chem.rdmolfiles.MolFromXYZFile(file_path)
 
 
 print(file_path)
